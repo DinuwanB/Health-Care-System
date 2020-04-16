edabb657fd6c38d3ddde339073c067b26f54bf73
DinuwanB <dinubhagya97@gmail.com>
2020-04-17 01:03:59 +0530

    BASE64 dependency and auth java class added

---
+++ Project/HealthManagement/src/main/java/com/pafProject/HealthManagement/BasicAuth.java
@@ -0,0 +1,19 @@
+package com.pafProject.HealthManagement;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+
+public class BasicAuth implements ContainerRequestFilter {
+	
+	private static final String AUTHORIZATION_HEADER = "Authorization";
+	
+	@Override
+	public void filter(ContainerRequestContext requestContext) throws IOException {
+		
+		List<>
+		
+	}
+
+}
